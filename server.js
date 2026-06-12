69331a08558be57f390c579ed6e26b1b60b46a16
TheGreenMan42 <sussussssu@gmail.com>
2026-06-12 15:03:59 +0300

    апв
    ап

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-// простой HTTP-сервер (Render этого требует)
 const server = http.createServer((req, res) => {
   res.writeHead(200);
   res.end("Pixel server is running");
@@ -14,32 +13,21 @@ const wss = new WebSocket.Server({ server });
 let pixels = Array(25 * 25).fill("#ffffff");
 
 wss.on("connection", ws => {
-  // отправляем текущее состояние новому клиенту
   ws.send(JSON.stringify({ type: "init", pixels }));
 
   ws.on("message", msg => {
     let data;
-    try {
-      data = JSON.parse(msg);
-    } catch {
-      return;
-    }
+    try { data = JSON.parse(msg); } catch { return; }
 
     if (data.type === "paint") {
-      const index = data.index;
-      const color = data.color;
+      pixels[data.index] = data.color;
 
-      if (index < 0 || index >= pixels.length) return;
-
-      pixels[index] = color;
-
-      // рассылаем всем
       wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(JSON.stringify({
             type: "update",
-            index,
-            color
+            index: data.index,
+            color: data.color
           }));
         }
       });
